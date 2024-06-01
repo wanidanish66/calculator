fe8289dc434b6269c94da7cb1af503de53e76882
wanidanish66 <xaidwani89@gmail.com>
2024-06-01 21:43:06 +0530

    added functionality in javaScript

---
+++ script.js
@@ -0,0 +1,37 @@
+let currentInput = '';
+let operator = null;
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    operator = op;
+    currentInput += op;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    operator = null;
+    updateDisplay();
+}
+
+// function appendDecimal(dot) {
+//     if (!currentInput.includes(dot)) {
+//         currentInput += dot;
+//         updateDisplay();
+//     }
+// }
+
+function calculate() {
+    let result = eval(currentInput);
+    currentInput = String(result);
+    operator = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').value = currentInput;
+}
